01eb7319b8ec3fa94f1833142bbcba5a9f519d0d
Priyanshu <psneb420@gmail.com>
2024-04-27 17:04:34 +0545

    Removed dependencies

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
   return <View></View>;
